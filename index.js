f37a423f32ec7b773c4905d21fb63ac631363f6e
bySabi Files <bysabi.files@gmail.com>
2017-07-19 12:05:56 +0200

    Initial commit

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ function z (x) {
 }
 
 function loop (x, fns) {
-  for (var i = 0, fn = fns[0]; i < fns.length;) {
+  for (var i = 0, fn = fns[0], r; i < fns.length;) {
     if (!isFunction(fn)) return fn
-    var r = fn(x)                  // 'r' is AssertCB type ?
+    r = fn(x)                  // 'r' is AssertCB type ?
     if (!isAssertCB(r)) return r
     fn = r.p                       // where to iterate:
       ? r.fn                       //  -- x (right?)
       : fns[++i]                   // |
-                                   // y (down?)
-  }
+  }                                // y (down?)
 }
 
 module.exports = z
 
-function isFunction(x) { 
+function isFunction (x) {
   return typeof x === 'function'
 }
 
-function isAssertCB(x) { 
+function isAssertCB (x) {
   return x && x[AssertCB.prototype.type]
 }
