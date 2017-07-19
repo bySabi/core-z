459d3dad39de0d34a5b8079c3bc3f59b1aa40810
bySabi Files <bysabi.files@gmail.com>
2017-07-19 12:09:09 +0200

    Initial commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function z (x) {
 function loop (x, fns) {
   for (var i = 0, fn = fns[0], r; i < fns.length;) {
     if (!isFunction(fn)) return fn
-    r = fn(x)                  // 'r' is AssertCB type ?
+    r = fn(x)                      // 'r' is AssertCB type ?
     if (!isAssertCB(r)) return r
     fn = r.p                       // where to iterate:
       ? r.fn                       //  -- x (right?)
