de531a812cd2e1c44dfee978bedad72ca5c554be
bySabi Files <bysabi.files@gmail.com>
2017-07-24 12:35:41 +0200

    add multiple xs arguments support

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function z () {
   }
 }
 
-function loop (fns, xs) {
+function loop (fs, xs) {
   var r
   var i = 0
-  var fn = fns[0]
-  while (i < fns.length) {
-    if (!isFunction(fn)) return fn
-    r = fn.apply(null, xs)
+  var f = fs[0]
+  while (i < fs.length) {
+    if (!isFunction(f)) return f
+    r = f.apply(null, xs)
     if (r) {
       // r is an assertcb Type?
       if (r._ToF_ === true) {               // where to iterate:
-        fn = r.fn                           //  -- x (right?)
+        f = r.fn                           //  -- x (right?)
       } else if (r._ToF_ === false) {       // |
-        fn = fns[++i]                       // y (down?)
+        f = fs[++i]                       // y (down?)
       } else return r
     } else return r
   }
